7f8cdd3acad1a8491cc7aebd1f3868681a42b46d
Lingyj <lingyj@iiseeuu.com>
2014-09-20 12:42:53 +0800

    commit

---
+++ Draw/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Draw
+//
+//  Created by student on 12-8-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
